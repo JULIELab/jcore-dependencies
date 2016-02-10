3be4d7d70bc9a09f717e66faf459167464981e7e
AllesEins <franz.matthies@uni-jena.de>
2016-02-10 14:21:17 +0100

    minuscle change

--- biosem-event-extractor/src/application/BioSemApplication.java
+++ biosem-event-extractor/src/application/BioSemApplication.java
@@ -1,10 +1,6 @@
 /**
  * BioSemApplication.java
  *
- * Copyright (c) 2015, JULIE Lab.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
- *
  * Author: matthies
  *
  * Current version: //TODO insert current version number
