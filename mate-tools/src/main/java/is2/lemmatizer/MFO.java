5bd23898bfed907e01f74c7f98cf1a002b8f03c3
khituras <chew@gmx.net>
2017-03-09 17:44:00 +0100

    Setting the correct parent for the java8 doclinter deactivation. Fixed / removed some misplaced javadoc items in the mate project because they were causing javadoc errors regardless of deactivated doclinter. Also, added a jar-plugin configuration to exclude the package.html from packaging for the mate project because this causes another error of duplicated package docs (they are not required / wanted with the class files). This is also added to julielab-parent v 2.1.0-SNAPSHOT for the future.

--- mate-tools/src/main/java/is2/lemmatizer/MFO.java
+++ mate-tools/src/main/java/is2/lemmatizer/MFO.java
@@ -176,8 +176,6 @@ final public class MFO  implements IEncoderPlus {
    
 	/** 
 	 * Maps a long to a integer value. This is very useful to save memory for sparse data long values 
-	 * @param l
-	 * @return the integer
 	 */
 	static public  int misses = 0;
 	static public  int good = 0;
