762de9db816f865f181201dd23b90e34d3402381
khituras <chew@gmx.net>
2021-06-21 13:42:52 +0200

    jcore-db-test-utilities: Version 2.5.2
    Added dummy classes to replace jUnit 4 rules which are a dependency of testcontainers.

---
+++ jcore-db-test-utilities/src/main/java/org/junit/rules/TestRule.java
@@ -0,0 +1,8 @@
+package org.junit.rules;
+
+/**
+ * This is a dummy class to allow the usage of testcontainers for jUnit5 while completely excluding jUnit4.
+ */
+@SuppressWarnings("unused")
+public interface TestRule {
+}
---
+++ jcore-db-test-utilities/src/main/java/org/junit/runners/model/Statement.java
@@ -0,0 +1,8 @@
+package org.junit.runners.model;
+
+/**
+ * This is a dummy class to allow the usage of testcontainers for jUnit5 while completely excluding jUnit4.
+ */
+@SuppressWarnings("unused")
+public class Statement {
+}
